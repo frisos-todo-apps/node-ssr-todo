d5ca6ba5f4e6aa5b069429e1ea961f4871381f1b
Friso Denijs <friso.artbuff@outlook.com>
2022-03-16 10:18:21 +0100

    added pug

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log("hello world!");
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const app = express();
+
+app.set('views', path.resolve('src/views')); // this directs the rendering to the views folder
+app.set('view engine', 'pug'); // with this I don't need to add the '.pug' extension in the render target 
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  })
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log('Server is up and running on port ' + port);
+});
