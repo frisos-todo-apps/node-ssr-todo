186ea8426e55c9041ac55642c0d390c46ff5a13a
Friso Denijs <friso.artbuff@outlook.com>
2022-03-16 11:23:33 +0100

    getting list from file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const fs = require('fs');
+
+const dataPath = './data/list.json';
 
 const app = express();
 
@@ -11,20 +14,17 @@ const createTodo = (isDone, description) => {
     }
 }
 
-const todoList = [
-    createTodo(true, 'setup project'),
-    createTodo(true, 'mock todo list'),
-    createTodo(false, 'create pug page'),
-    createTodo(false, 'use express for POST and put'),
-    createTodo(false, 'save list to and get from file'),
-    createTodo(false, 'prettify with bootstrap'),
-]
+const getList = () => JSON.parse(fs.readFileSync(dataPath));
 
 app.set('views', path.resolve('src/views')); // this directs the rendering to the views folder
 app.set('view engine', 'pug'); // with this I don't need to add the '.pug' extension in the render target 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!', list: todoList })
+    res.render('index', { title: 'Hey', message: 'Hello there!', list: getList() })
   })
+
+app.post('/todo', (req, res) => {
+    
+});
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
