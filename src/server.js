e0f4e5a10de36865d28b68bae5e8231ba251faca
Friso Denijs <friso.artbuff@outlook.com>
2022-03-16 10:51:23 +0100

    added input to pug file

--- src/server.js
+++ src/server.js
@@ -4,10 +4,26 @@ const router = express.Router();
 
 const app = express();
 
+const createTodo = (isDone, description) => {
+    return {
+        isDone,
+        description
+    }
+}
+
+const todoList = [
+    createTodo(true, 'setup project'),
+    createTodo(true, 'mock todo list'),
+    createTodo(false, 'create pug page'),
+    createTodo(false, 'use express for POST and put'),
+    createTodo(false, 'save list to and get from file'),
+    createTodo(false, 'prettify with bootstrap'),
+]
+
 app.set('views', path.resolve('src/views')); // this directs the rendering to the views folder
 app.set('view engine', 'pug'); // with this I don't need to add the '.pug' extension in the render target 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', { title: 'Hey', message: 'Hello there!', list: todoList })
   })
 
 const port = process.env.PORT || 3000;
